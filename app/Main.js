f6ab4e3e8b856002f0efce19a2d023a50c556b02
Kriss <krissfrandsen@gmail.com>
2020-11-17 20:28:40 +0100

    setting up workflow

---
+++ app/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const ExampleComponent = () => {
+  return (
+    <div>
+      <h1>This is our App</h1>
+      <p>hello</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<ExampleComponent />, document.quesrySelector("#app"))
