791497e572e9be4f1b2ba75b3ee042d6b083c2e5
Kriss <krissfrandsen@gmail.com>
2020-12-12 16:56:03 +0100

    setting up localStorage

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -10,7 +10,9 @@ const HeaderLoggedOut = ({ setLoggedIn }) => {
     try {
       const response = await Axios.post("http://localhost:8080/login", { username, password });
       if (response.data) {
-        console.log(response.data);
+        localStorage.setItem("complexappToken", response.data.token);
+        localStorage.setItem("complexappUsername", response.data.username);
+        localStorage.setItem("complexappAvatar", response.data.avatar);
         setLoggedIn(true);
       } else {
         console.log("Incorrect username and password");
